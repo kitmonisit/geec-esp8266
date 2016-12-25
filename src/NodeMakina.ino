dbb5ff0eec7632032ff51484943b613e09f3a7cf
Kristofer Monisit <kristofer.monisit@analog.com>
2016-12-25 23:45:43 +0800

    Use TickerScheduler

--- src/NodeMakina.ino
+++ src/NodeMakina.ino
@@ -1,4 +1,5 @@
 #include <ESP8266WiFi.h>
+#include <TickerScheduler.h>
 #include <sodium.h>
 #include <frozen.h> // JSON emitter and parser
 
@@ -8,6 +9,7 @@
 #define LED_BLUE      (uint8_t) 2
 
 // EspClass esp;
+TickerScheduler ts(1);
 
 static const char *func_name(void);
 static void        func_random_buf(void * const buf, const size_t size);
@@ -53,37 +55,13 @@ void setup() {
 
     randombytes_set_implementation(&impl);
     sodium_init();
+
+    ts.add(0, 1000, routine_LupangHinirang);
 }
 
 void loop() {
-    delay(1000);
+    ts.update();
     // Serial.printf("%d free RAM\n", esp.getFreeHeap());
-    stream_begin();
-    stream_add("Bayang Magiliw,\n");
-    stream_add("Perlas ng Silanganan\n");
-    stream_add("Alab ng puso\n");
-    stream_add("Sa Dibdib mo'y buhay.\n");
-    stream_add("\n");
-    stream_add("Lupang Hinirang,\n");
-    stream_add("Duyan ka ng magiting,\n");
-    stream_add("Sa manlulupig\n");
-    stream_add("Di ka pasisiil\n");
-    stream_add("\n");
-    stream_add("Sa dagat at bundok,\n");
-    stream_add("Sa simoy at sa langit mong bughaw,\n");
-    stream_add("May dilag ang tula\n");
-    stream_add("At awit sa paglayang minamahal.\n");
-    stream_add("\n");
-    stream_add("Ang kislap ng watawat mo'y\n");
-    stream_add("Tagumpay na nagniningning;\n");
-    stream_add("Ang bituin at araw niya,\n");
-    stream_add("Kailan pa ma'y di magdidilim\n");
-    stream_add("\n");
-    stream_add("Lupa ng araw, ng luwalhati't pagsinta,\n");
-    stream_add("Buhay ay langit sa piling mo;\n");
-    stream_add("Aming ligaya na pag may mang-aapi,\n");
-    stream_add("Ang mamatay nang dahil sa iyo.\n");
-    stream_end();
 }
 
 // vim:fdm=syntax
---
+++ src/routines.ino
@@ -0,0 +1,29 @@
+void routine_LupangHinirang(void)
+{
+    stream_begin();
+    stream_add("Bayang Magiliw,\n");
+    stream_add("Perlas ng Silanganan\n");
+    stream_add("Alab ng puso\n");
+    stream_add("Sa Dibdib mo'y buhay.\n");
+    stream_add("\n");
+    stream_add("Lupang Hinirang,\n");
+    stream_add("Duyan ka ng magiting,\n");
+    stream_add("Sa manlulupig\n");
+    stream_add("Di ka pasisiil\n");
+    stream_add("\n");
+    stream_add("Sa dagat at bundok,\n");
+    stream_add("Sa simoy at sa langit mong bughaw,\n");
+    stream_add("May dilag ang tula\n");
+    stream_add("At awit sa paglayang minamahal.\n");
+    stream_add("\n");
+    stream_add("Ang kislap ng watawat mo'y\n");
+    stream_add("Tagumpay na nagniningning;\n");
+    stream_add("Ang bituin at araw niya,\n");
+    stream_add("Kailan pa ma'y di magdidilim\n");
+    stream_add("\n");
+    stream_add("Lupa ng araw, ng luwalhati't pagsinta,\n");
+    stream_add("Buhay ay langit sa piling mo;\n");
+    stream_add("Aming ligaya na pag may mang-aapi,\n");
+    stream_add("Ang mamatay nang dahil sa iyo.\n");
+    stream_end();
+}
